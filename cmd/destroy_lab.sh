598c26e165cc3c9e0de11f41e71d4d2f1895b417
nazliozbekk <nazliozbek14@gmail.com>
2025-08-27 11:21:06 +0300

    cmd

---
+++ cmd/destroy_lab.sh
@@ -0,0 +1,12 @@
+cd "$(dirname "$0")/.."
+
+docker run --rm -it \
+  --privileged \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME/.containerlab:/root/.containerlab" \
+  -v "$(pwd):/lab" \
+  --network host \
+  ghcr.io/srl-labs/clab bash -c "
+    cd /lab &&
+    containerlab destroy -t lab.yml -c
+  "
